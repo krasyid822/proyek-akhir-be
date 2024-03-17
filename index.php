a9745ed224bb797cd6aa090507815ac4d70627b9
Muhammad Farid Yamin <muhammad.farid.yamin@gmail.com>
2024-03-17 15:25:20 +0700

    first commit

---
+++ index.php
@@ -0,0 +1,20 @@
+<?php
+define('HOST', 'localhost');
+define('USERNAME', 'root');
+define('PASSWORD', '');
+define('DB_NAME', 'daftar_peserta_codify');
+
+$connection = mysqli_connect(HOST, USERNAME, PASSWORD, DB_NAME);
+
+$query = mysqli_query($connection, 'select * from daftar_peserta;');
+
+$rows = [];
+?>
+<table border=1>
+  <?php
+      while ($data = mysqli_fetch_array($query)) {
+        echo "<tr><td>". $data["nama_peserta"] ."</td></tr>";
+      }
+      mysqli_close($connection);
+    ?>
+</table>
